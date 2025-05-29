26d65ce88cc75e2c2f90e3a28f384be8309e580d
lucasoriental <lucasoriental16@gmail.com>
2025-05-29 11:20:39 +0100

    Fixing the tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,10 @@ const config = {
   theme: {
     screens: {
       xs: "360px",
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px",
-      "3xl": "1920px",
-      "4k": "2560px",
     },
     extend: {
       backgroundImage: {
-        "homeSections": "url('/images/fundo.jpg')",
-      },
-      colors: {
-        HiringLightPrimary: "#E23030",
-        HiringLightSecondary: "#9A0404",
+        homeSections: "url('/images/fundo.jpg')",
       },
       fontFamily: {
         sans: ["var(--font-geologica)"],
