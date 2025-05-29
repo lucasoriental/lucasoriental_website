c9794e92635c715481295fe162991e7d0237eb9f
lucasoriental <lucasoriental16@gmail.com>
2025-05-29 11:19:08 +0100

    Installing Lucide for icons and fixing tailwind configuration. Also adding new settings in the tailwind.config, adding a small breakpoint for smaller devices; Background image for home page and new font for the website.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,28 @@
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    screens: {
+      xs: "360px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "1920px",
+      "4k": "2560px",
+    },
+    extend: {
+      backgroundImage: {
+        "homeSections": "url('/images/fundo.jpg')",
+      },
+      colors: {
+        HiringLightPrimary: "#E23030",
+        HiringLightSecondary: "#9A0404",
+      },
+      fontFamily: {
+        sans: ["var(--font-geologica)"],
+      },
+    },
   },
   plugins: [],
   darkMode: "class",
